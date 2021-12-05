36e165a287e33ec457a3ec6897267aead6d43ccd
Daviey52 <davidkariska@gmail.com>
2021-12-05 09:37:39 -0800

    edit03

--- snales_cafe/snakes_cafe.py
+++ snales_cafe/snakes_cafe.py
@@ -1,43 +1,46 @@
-print("""
-**    Welcome to the Snakes Cafe!   **
-**    Please see our menu below.    **
-**
-** To quit at any time, type "quit" **
-**************************************
+def menu_items():
+    print("""
+  **    Welcome to the Snakes Cafe!   **
+  **    Please see our menu below.    **
+  **
+  ** To quit at any time, type "quit" **
+  **************************************
 
-""")
-print("""
-Appetizers
-----------
-Wings
-Cookies
-Spring Rolls
+  """)
+    print("""
+  Appetizers
+  ----------
+  Wings
+  Cookies
+  Spring Rolls
 
-Entrees
--------
-Salmon
-Steak
-Meat Tornado
-A Literal Garden
+  Entrees
+  -------
+  Salmon
+  Steak
+  Meat Tornado
+  A Literal Garden
 
-Desserts
---------
-Ice Cream
-Cake
-Pie
+  Desserts
+  --------
+  Ice Cream
+  Cake
+  Pie
 
-Drinks
-------
-Coffee
-Tea
-Unicorn Tears
+  Drinks
+  ------
+  Coffee
+  Tea
+  Unicorn Tears
 
-""")
-print("""
-***********************************
-** What would you like to order? **
-***********************************
-""")
+  """)
+    print("""
+  ***********************************
+  ** What would you like to order? **
+  ***********************************
+  """)
+
+
 menu = [
     "Wings",
     "Cookies",
@@ -53,14 +56,24 @@ menu = [
     "Tea", "Unicorn Tears",
 ]
 
+if __name__ == "__main__":
+    menu_items()
 
-def menu_input():
-    user_choice = input("> ")
-    for menu in range(1):
-        if user_choice == "quit":
-            print("Exiting have a good day")
-        if user_choice == menu:
-            print(f" 1 order of {user_choice} have been added to your meal")
+    def menu_input():
+        user_choice = input("> ")
+        items_ordered = []
+        count = 1
+        while user_choice:
+            if user_choice == "quit":
+                print("Exiting have a good day")
+                break
+            if user_choice != "quit":
+                items_ordered.append(user_choice)
+                if user_choice == items_ordered:
+                    count += 1
+                print(
+                    f" {count} order of {user_choice} has been added to your meal")
+                user_choice
+                break
 
-
-menu_input()
+    menu_input()
