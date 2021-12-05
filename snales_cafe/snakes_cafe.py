74b5bd60407e6d68bc3837c8764fe7477c4e2988
Daviey52 <davidkariska@gmail.com>
2021-12-04 17:53:57 -0800

    first edits

---
+++ snales_cafe/snakes_cafe.py
@@ -0,0 +1,66 @@
+print("""
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+""")
+print("""
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+""")
+print("""
+***********************************
+** What would you like to order? **
+***********************************
+""")
+menu = [
+    "Wings",
+    "Cookies",
+    "Spring Rolls",
+    "Salmon",
+    "Steak",
+    "Meat Tornado",
+    "A Literal Garden",
+    "Ice Cream",
+    "Cake",
+    "Pie",
+    "Coffee",
+    "Tea", "Unicorn Tears",
+]
+
+
+def menu_input():
+    user_choice = input("> ")
+    for menu in range(1):
+        if user_choice == "quit":
+            print("Exiting")
+        if user_choice == menu:
+            print(f" 1 order of {user_choice} have been added to your meal")
+
+
+menu_input()
